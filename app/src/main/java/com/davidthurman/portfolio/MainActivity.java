f9d7d53417329d973d89631394c29b7db52bbec4
= <davidthurmanwork@gmail.com>
2019-02-13 22:25:18 -0500

    Tab bar working

--- app/src/main/java/com/davidthurman/portfolio/MainActivity.java
+++ app/src/main/java/com/davidthurman/portfolio/MainActivity.java
@@ -1,15 +1,18 @@
 package com.davidthurman.portfolio;
 
-import android.support.annotation.NonNull;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private int currentMenuItem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,37 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.pager);
         viewPager.setAdapter(adapter);
+
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home_menu_item:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.interests_menu_item:
+                        viewPager.setCurrentItem(1);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(currentMenuItem).setChecked(false);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                currentMenuItem = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) { }
+        });
     }
 }
 
--- app/src/main/java/com/davidthurman/portfolio/MyPagerAdapter.java
+++ app/src/main/java/com/davidthurman/portfolio/MyPagerAdapter.java
@@ -1,8 +1,8 @@
 package com.davidthurman.portfolio;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import com.davidthurman.portfolio.Home.HomeFragment;
 import com.davidthurman.portfolio.Interests.InterestsFragment;
